cbc499be5f6a4f7a73bf0c3521aa1665bb843bac
dascolari <dascolari@utexas.edu>
2022-02-17 18:10:34 -0600

    i think i'm done

--- R/output/incarceration_bar.R
+++ R/output/incarceration_bar.R
@@ -1,12 +1,12 @@
 read_csv(here(file.path("data", "nlsy97_clean.csv"))) %>%
   filter(race != "Mixed Race (Non-Hispanic)") %>% 
   group_by(race, gender) %>%
-  summarise(mean_days_incarcerated = (sum(months_incarcerated)*(365/12)*(1/n())), n = n(), .groups = 'drop') %>% 
+  summarise(mean_days_incarcerated = (mean(months_incarcerated)*(365/12)), n = n(), .groups = 'drop') %>% 
   pivot_wider(names_from = gender, 
               values_from = mean_days_incarcerated, 
               id_cols = race) %>% 
   mutate(ratio = Male/Female) %>% 
   ggplot() +
   geom_col(aes(x = race, y = ratio), fill = '#8668FF') +
-  labs(x = "Race", y = "Male/Female Ratio", title = "Ratio of Male/Female Incarceration Time per Person")
+  labs(x = "Race", y = "Male/Female Ratio", title = "Ratio of Male/Female per Person Incarceration Time")
 ggsave(here(file.path("figures", "inc_bar.png")), width=8, height=4.5)
--- R/output/incarceration_table.R
+++ R/output/incarceration_table.R
@@ -1,7 +1,7 @@
 read_csv(here(file.path("data", "nlsy97_clean.csv"))) %>%
   filter(race != "Mixed Race (Non-Hispanic)") %>% 
   group_by(race, gender) %>%
-  summarise(mean_days_incarcerated = (sum(months_incarcerated)*(365/12)*(1/n())), n = n()) %>% 
+  summarise(mean_days_incarcerated = mean(months_incarcerated)*(365/12), n = n()) %>% 
   pivot_wider(names_from = gender, 
               values_from = mean_days_incarcerated, 
               id_cols = race) %>% 
@@ -9,7 +9,7 @@ read_csv(here(file.path("data", "nlsy97_clean.csv"))) %>%
   
   # create the kable object. Requires booktabs and float LaTeX packages
   kbl(
-    caption = "Per Person Incaeration Days",
+    caption = "Per Person Incarceration Days",
     booktabs = TRUE,
     format = "latex",
     label = "tab:summarystats"
